9bd7274d0fbd02076ae3a84c886ae65813310c2f
Douglas Morais <douglasmorais9915@outlook.com>
2023-08-16 21:08:26 -0300

    Backend base created

---
+++ backend/src/test/java/patient/scheduling/system/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package patient.scheduling.system.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
