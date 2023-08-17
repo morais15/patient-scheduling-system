79b7a15eb5a13693bceed9a1a57df6618da188b4
Douglas Morais <douglasmorais9915@outlook.com>
2023-08-16 21:35:20 -0300

    Configured database and swagger

---
+++ backend/src/main/java/patient/scheduling/system/api/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package patient.scheduling.system.api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Patient Scheduling System API"))
+public class SwaggerConfig {
+}
