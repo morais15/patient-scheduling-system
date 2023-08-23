c925626525d8c3f0fb54cf859187473770982957
Douglas Morais <douglasmorais9915@outlook.com>
2023-08-23 15:23:44 -0300

    Configured environment variables

--- frontend/src/app/health-units/service/health-units.service.ts
+++ frontend/src/app/health-units/service/health-units.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { HealthUnit } from '../domain/health-unit';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HealthUnitsService {
+  private readonly API_URL: String = environment.API_URL;
 
   constructor(private httpClient: HttpClient) { }
 
   listAll(): Observable<HealthUnit[]> {
-    return this.httpClient.get<HealthUnit[]>('')
+    return this.httpClient.get<HealthUnit[]>(`${this.API_URL}/health-units`)
   }
 }
